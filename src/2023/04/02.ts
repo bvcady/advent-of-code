a11db040c3647ceda7fc56f4aa74f2f6da0bbce1
Bob Verheij <bobverheij@Bobs-MBP-2.home>
2023-12-04 10:04:54 +0100

    Day 4 part 2

---
+++ src/2023/04/02.ts
@@ -0,0 +1,60 @@
+import * as prior from "./01";
+
+export const solution = (file: string): string | number => {
+  const input = prior.getInput(file);
+
+  const cards = input.map((line) => {
+    const game = Number(
+      line.split(":")?.[0]?.trim()?.split(" ")?.slice(-1)?.[0]
+    );
+    const yourNumbers = line
+      .split(":")?.[1]
+      ?.trim()
+      ?.split("|")?.[0]
+      ?.trim()
+      ?.split(" ")
+      ?.filter((n) => !!n);
+    const winningNumbers = line
+      .split(":")?.[1]
+      ?.trim()
+      ?.split("|")?.[1]
+      ?.trim()
+      ?.split(" ")
+      ?.filter((n) => !!n);
+    const nWinning = yourNumbers.filter((n) => winningNumbers?.includes(n))
+      ?.length;
+    return {
+      game,
+      yourNumbers,
+      winningNumbers,
+      newCards: new Array(nWinning)
+        .fill("")
+        .map((_, index) => game + 1 + index),
+    };
+  });
+
+  const cardDistribution: Record<string, { n: number; nWinning: number }> =
+    new Array(input.length).fill("").reduce((acc, _, index) => {
+      return {
+        ...acc,
+        [index + 1]: {
+          n: 1,
+          nWinning: cards.find((c) => c.game === index + 1)?.newCards?.length,
+        },
+      };
+    }, {});
+
+  cards?.forEach((card) => {
+    const game = card.game;
+    const nDis = cardDistribution[game]?.n;
+
+    for (let i = 0; i < nDis; i++) {
+      card.newCards.forEach((_c) => (cardDistribution[_c].n += 1));
+    }
+  });
+
+  return Object.keys(cardDistribution).reduce(
+    (tot, cur) => (tot += cardDistribution[cur].n),
+    0
+  );
+};
