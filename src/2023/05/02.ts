cc51074f36fddc5830a0158611274d19118b7d74
Bob Verheij <bobverheij@Bobs-MBP-2.home>
2023-12-06 08:39:46 +0100

    Non-fixes of day 5 and start of day 6

--- src/2023/05/02.ts
+++ src/2023/05/02.ts
@@ -1,3 +1,4 @@
+import { start } from "repl";
 import * as prior from "./01";
 
 export const solution = (file: string): string | number => {
@@ -13,9 +14,12 @@ export const solution = (file: string): string | number => {
     const length = Number(r.split(" ")?.[1].trim());
 
     return {
-      start,
-      length,
-      end: start + length - 1,
+      seeds: [
+        {
+          start,
+          end: start + length - 1,
+        },
+      ],
     };
   });
 
@@ -40,7 +44,85 @@ export const solution = (file: string): string | number => {
     };
   });
 
+  console.log(seedRanges?.length);
+
   let lowest = Infinity;
+  const withDepth = seedRanges?.map((range) => {
+    let currentRange: Record<string, Record<"start" | "end", number>[]> = range;
+    everyMap.forEach((map, index) => {
+      console.log(map.text);
+      currentRange[map.text] = currentRange[
+        index > 0 ? everyMap[index - 1].text : "seeds"
+      ].reduce(
+        (collection, range) => {
+          let oldRanges = [] as Record<"start" | "end", number>[];
+          // console.log({ rangeStart: range.start, rangeEnd: range.end });
+          let addedRanges = [] as Record<"start" | "end", number>[];
+          map.values.forEach((val) => {
+            let newRange = {} as Record<"start" | "end", number>;
+
+            const startVal = val.srs;
+            const endVal = val.srs + val.rl - 1;
+
+            // console.log({ startVal, endVal });
+
+            if (startVal <= range.end && endVal >= range.start) {
+              const deltaStart =
+                startVal - range.start >= 0 ? startVal - range.start : 0;
+              const deltaEnd =
+                range.end >= val.srs + val.rl - 1
+                  ? val.srs + val.rl - 1 - range.end
+                  : 0;
+
+              // console.log({ deltaStart, deltaEnd });
+
+              const delta = val.drs - val.srs;
+
+              newRange.start = deltaStart + range.start + delta;
+              newRange.end = range.end - deltaEnd + delta;
+
+              const oldRange = {
+                start: deltaStart + range.start,
+                end: range.end - deltaEnd,
+              };
+
+              oldRanges.push(oldRange);
+              addedRanges.push(newRange);
+            } else {
+              // console.log("Vals not in range");
+            }
+          });
+
+          if (addedRanges.length) {
+            const slices = range;
+            const remainder = [] as Record<"start" | "end", number>[];
+            console.log({ oldRanges });
+            console.log({ addedRanges });
+            oldRanges
+              .sort((a, b) => a.start - b.start)
+              .forEach((r, index) => {
+                let newRemainder = { start: 0, end: 0 };
+                if (r.start !== slices.start) {
+                  newRemainder.start = slices.start;
+                  newRemainder.end = r.start - 1;
+                  slices.start = r.end + 1;
+                  slices.end = oldRanges[index]?.start - 1;
+                  remainder.push(newRemainder);
+                }
+              });
+            console.log({ remainder });
+
+            return [...collection, ...addedRanges, ...remainder];
+          } else {
+            return [...collection, range];
+          }
+        },
+        [] as Record<"start" | "end", number>[]
+      );
+    });
+    return currentRange;
+  });
+  console.log(JSON.stringify(withDepth, null, 2));
 
   const finalLocations = (item: number) => {
     let newValue = item;
@@ -64,12 +146,6 @@ export const solution = (file: string): string | number => {
       lowest = newValue;
     }
   };
-
-  seedRanges?.forEach((range) => {
-    for (let i = range.start; i <= range.end; i++) {
-      finalLocations(i);
-    }
-  });
 
   return lowest;
 };
