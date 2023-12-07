59863c33a67df8cc19555c7f6a3b3179e97cbbb6
Bob Verheij <bobverheij@Bobs-MBP-2.home>
2023-12-07 08:56:03 +0100

    Day 6

--- src/2023/06/02.ts
+++ src/2023/06/02.ts
@@ -2,5 +2,39 @@ import * as prior from "./01";
 
 export const solution = (file: string): string | number => {
   const input = prior.getInput(file);
+
+  const raceDetails = input.reduce(
+    (tot, line) => {
+      return {
+        ...tot,
+        [line.split(":")?.[0].trim()]:
+          line.split(":")?.[1].match(/(\d)+/g) || [],
+      };
+    },
+    {} as Record<string, string[]>
+  );
+
+  const time = raceDetails.Time.join("");
+  const distance = raceDetails.Distance.join("");
+
+  console.log(time, distance);
+
+  const checkRace = (race: { time: string; distance: string }) => {
+    let d = Number(race.time) - 1;
+    let maxHoldTime = Number(race.time) - 1;
+    let winningSolutions = [] as number[];
+    while (d >= 1) {
+      let newDistance = d + d * (maxHoldTime - d);
+      if (newDistance > Number(race.distance)) {
+        winningSolutions.push(newDistance);
+      }
+      d--;
+    }
+
+    return winningSolutions.length;
+  };
+
+  const longResult = checkRace({ time, distance });
+  console.log(longResult);
   return "";
 };
